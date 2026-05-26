6a41f3e7fad610922455c86c443899ff5dad4d04
MinatoHayashi <occ3710@outlook.jp>
2026-05-26 17:20:59 +0900

    feat: setup due to vite 8

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,10 @@
-import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite'; // 💡 追加
 
-export default defineConfig({ plugins: [tailwindcss(), sveltekit()] });
+export default defineConfig({
+	plugins: [
+		tailwindcss(), // sveltekit() より前に配置
+		sveltekit()
+	]
+});
